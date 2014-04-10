f48a046ad2a44b5b6be9bee62dbc8040156dbd20
Jeremy Burns <jeremyaburns@gmail.com>
2014-04-10 17:46:50 +0000

    Replaced infinite loop with recursive call to script

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while :;do echo|read|{ (read t;g=$(echo "$t"|cut -d\  -f2)
+echo|read|{(read t;g=$(echo "$t"|cut -d\  -f2)
 while read|grep :;do :;done;[ -e ".$g" ]||exit
 printf "HTTP/1.1 200 OK\nContent-Length: $(stat -c%s ".$g")\n\n"
-cat ".$g")|nc -l $1;}>/dev/fd/0;done
+cat ".$g")|nc -l $1;}>/dev/fd/0;$0 $1
