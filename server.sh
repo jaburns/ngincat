826aa05b2992fc2d1b5044b8e1ba853f1db97bb5
Jeremy Burns <j@jaburns.net>
2014-07-17 10:56:51 -0600

    Tweaked to work on arch
    http://www.reddit.com/r/tinycode/comments/2awnv0/bash_web_server_using_netcat_in_209_bytes/cizwc6s

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo|read|{(read t;g=$(echo "$t"|cut -d\  -f2)
+echo|read|{(read t;g=$(echo "$t"|cut -d' ' -f2)
 while read|grep :;do :;done;[ -e ".$g" ]||exit
 printf "HTTP/1.1 200 OK\nContent-Length: $(stat -c%s ".$g")\n\n"
-cat ".$g")|nc -l $1;}>/dev/fd/0;$0 $1
+cat ".$g")|nc -l -p $1;}>/dev/fd/0;$0 $1
